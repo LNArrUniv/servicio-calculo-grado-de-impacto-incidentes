c2c11165d7ff98b47556708382334723dc8dc6e4
LNArrUniv <103150494+LNArrUniv@users.noreply.github.com>
2024-03-19 02:34:30 -0300

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 
   @Test
-  public void testAlgoQueDeberiaEstarBien() {
+  public void testCalculoDeEjemplo() {
     /*
     Calculo de ejemplo:
 
